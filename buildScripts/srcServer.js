86ec04c5dd0e3971909904045405df64e32d0506
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-01-06 16:52:30 +0100

    Making HTTP Calls with Fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get ('/', function (req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res){
+    res.json([
+    {"id": 1, "firstName": "Matthew", "lastName":"oye", "email": "ml@gmail.com"},
+    {"id": 2, "firstName": "Cornel", "lastName":"okon", "email": "co@gmail.com"},
+    {"id": 3, "firstName": "Osita", "lastName":"Irene", "email": "oi@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err){
     if (err){
         console.log(err);
