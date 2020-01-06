2dc403021f69f7470e4045b99de828847cd6bfa5
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-01-06 21:56:07 +0100

    Set Up Manipulating Data via JSON Server

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,20 +1,31 @@
 import "whatwg-fetch";
-import getBaseUrl from "./baseUrl";
+import getBaseUrl from './baseUrl';
 
-const baseUrl = getBaseUrl();
+const baseUrl = getBaseUrl ();
 
-export function getUsers() {
-    return get('users');
+export function getUsers (){
+  return get('users');
 }
 
-function get(url) {
+export function deleteUser(id){
+    return del(`users/${id}`);
+  }
+
+function get(url){
     return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
-function onSuccess(response) {
-    return response.json();
+function del (url) {
+    const request = new Request(baseUrl + url, {
+      method: "DELETE"
+    });
+    return fetch(request).then(onSuccess, onError);
+  }
+
+function onSuccess(response){
+  return response.json();
 }
 
-function onError(error) {
-    console.log(error); //eslint-disable-line no-console
+function onError(error){
+  console.log(error); //eslint-disable-line no-console
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import { getUsers } from "./api/userApi";
+import {getUsers, deleteUser} from "./api/userApi";
 
 //Populate table of users via API calls.
-getUsers().then(result => {
+getUsers().then (result => {
 
     let usersBody = "";
 
-    result.forEach(user => {
+    result.forEach(user =>{
         usersBody +=
-            `<tr>
-                <td> <a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-                <td>${user.id}</td>
-                <td>${user.firstName}</td>
-                <td>${user.lastName}</td>
-                <td>${user.email}</td>
-            </tr>`
+        `<tr>
+            <td> <a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+        </tr>`
     });
     global.document.getElementById('users').innerHTML = usersBody;
+
+
+    const deleteLinks = global.document.getElementsByClassName ('deleteUser');
+
+    //must use array.from to create a real array from a DOM collection
+    //getElementsByClassname only returns an 'array like' object
+    Array.from (deleteLinks, link => {
+        link.onclick = function (event){
+            const element = event.target;
+            event.preventDefault();
+            deleteUser (element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
